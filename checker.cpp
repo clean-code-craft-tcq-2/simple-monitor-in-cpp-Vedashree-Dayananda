4d1131ca676c32a07316c00153049afdf4b14e8e
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-02-08 22:13:38 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -11,27 +11,47 @@ const float SOC_MAX_THRESHOLD = 80.0F;
 const float CHARGERATE_MIN_THRESHOLD = 0.0F;
 const float CHARGERATE_MAX_THRESHOLD = 0.8F;
 
-bool valueInRange(const float minThreshold,const float maxThreshold,const float valueToCheck)
+//return value  0 - for value in range 
+//              1 - for value greater than threshold
+//             -1 - for value less than threshold
+int checkValueInRange(const float minThreshold,const float maxThreshold,const float valueToCheck)
 {
   if (valueToCheck < minThreshold)
   {
     cout << valueToCheck << "is less than Minimum Threshold value" << minThreshold <<endl;
-    return false;
+    return -1;
   }
   else if ( valueToCheck > maxThreshold)
   {
     cout << valueToCheck << "is greater than Maximum Threshold value" << maxThreshold <<endl;
-    return false;
+    return 1;
   }
-  return true;
+  return 0;
 }
 
+bool checkBatteryFactorStatus(const float minThreshold,const float maxThreshold,const float valueOfFactor,const char* factorname)
+{
+  int factorIndicator = checkValueInRange(minThreshold,maxThreshold,valueOfFactor);
+  bool result = false ;
+  switch(factorIndicator)
+  {
+    case 0 : result = true;
+             break;
+    case 1 : cout << factorname <<" : value : " << valueOfFactor << " is greater than the Maximum threshhold value " << maxThreshold <<endl;
+             result = false;
+             break;
+    case 1 : cout << factorname <<" : value : " << valueOfFactor << " is less than the Manimum threshhold value " << minThreshold <<endl;
+             result = false;
+             break;
+  }
+  return result;
+}
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
   bool temperatureIndicator = false,socIndicator = false,chargeRateIndicator = false ,result =false;
-  temperatureIndicator = valueInRange(TEMPERATURE_MIN_THRESHOLD,TEMPERATURE_MAX_THRESHOLD,temperature);
-  socIndicator = valueInRange(SOC_MIN_THRESHOLD,SOC_MAX_THRESHOLD,soc);
-  chargeRateIndicator = valueInRange(CHARGERATE_MIN_THRESHOLD,CHARGERATE_MAX_THRESHOLD,chargeRate);
+  temperatureIndicator = checkBatteryFactorStatus(TEMPERATURE_MIN_THRESHOLD,TEMPERATURE_MAX_THRESHOLD,temperature,"temperature");
+  socIndicator = checkBatteryFactorStatus(SOC_MIN_THRESHOLD,SOC_MAX_THRESHOLD,soc,"SOC");
+  chargeRateIndicator = checkBatteryFactorStatus(CHARGERATE_MIN_THRESHOLD,CHARGERATE_MAX_THRESHOLD,chargeRate,"ChargeRate");
   
   result = temperatureIndicator && socIndicator && chargeRateIndicator;
   return result;
