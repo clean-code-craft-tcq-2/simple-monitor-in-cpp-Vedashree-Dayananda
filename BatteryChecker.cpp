520fbf89dc47d4c1299a6aa390f2b6602d5f506b
Vedashree D <90906584+Vedashree-Dayananda@users.noreply.github.com>
2022-02-17 00:07:40 +0530

    Update BatteryChecker.cpp

--- BatteryChecker.cpp
+++ BatteryChecker.cpp
@@ -1,13 +1,14 @@
 #include <assert.h>
+#include <sstream>
 #include <iostream>
 using namespace std;
 #include "BatteryThresholdDefines.h"
 
-bool checkValueInRange(const float minThreshold, const float maxThreshold, const float valueToCheck, char** out_of_range_factor)
+bool checkValueInRange(const float minThreshold, const float maxThreshold, const float valueToCheck, string* out_of_range_factor)
 {
 	if (valueToCheck < minThreshold)
 	{
-		*out_of_range_factor = "low"; 
+		*out_of_range_factor = "low";
 		return false;
 	}
 	else if (valueToCheck > maxThreshold)
@@ -17,46 +18,96 @@ bool checkValueInRange(const float minThreshold, const float maxThreshold, const
 	}
 	return true;
 }
-
-void printOnConsole(const char* factorname, char* out_of_range_factor)
+bool checkValueInEarlyWarningRange(const float lowerLimit, const float upperLimit, const float value)
 {
-	cout << factorname << " value is " << out_of_range_factor << " than the threshhold value "<< endl;
+	if ((value >= lowerLimit) &&  (value <= upperLimit))
+		return true;
+	return false;
 }
-bool checkBatteryFactorStatus(const float minThreshold,const float maxThreshold,const float valueOfFactor,const char* factorname)
+void printOnConsole(string message)
 {
-	char* out_of_range_factor = "";
+	cout << message << endl;
+}
+bool checkBatteryFactorStatus(const float minThreshold, const float maxThreshold, const float minWarningThreshold, const float maxWarningThreshold, const float valueOfFactor, string factorname)
+{
+	string out_of_range_factor = "";
+	string message = "";
 	bool factorIndicator = checkValueInRange(minThreshold, maxThreshold, valueOfFactor, &out_of_range_factor);
 	if (!factorIndicator)
 	{
-		printOnConsole(factorname, out_of_range_factor);
+		message = factorname + " value is " + out_of_range_factor + " than the threshhold value ";
+		printOnConsole(message);
 	}
+	if (checkValueInEarlyWarningRange(minThreshold, minWarningThreshold ,valueOfFactor))
+	{
+		message = "Warning : " + factorname + " Approaching lower threshold";
+		printOnConsole(message);
+	}
+	else if (checkValueInEarlyWarningRange(maxWarningThreshold, maxThreshold, valueOfFactor))
+	{
+		message = "Warning : " + factorname + " Approaching Peak";
+		printOnConsole(message);
+	}
+
 	return factorIndicator;
 }
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  bool temperatureIndicator = false,socIndicator = false,chargeRateIndicator = false ,result =false;
-  temperatureIndicator = checkBatteryFactorStatus(TEMPERATURE_MIN_THRESHOLD,TEMPERATURE_MAX_THRESHOLD,temperature,"temperature");
-  socIndicator = checkBatteryFactorStatus(SOC_MIN_THRESHOLD,SOC_MAX_THRESHOLD,soc,"SOC");
-  chargeRateIndicator = checkBatteryFactorStatus(CHARGERATE_MIN_THRESHOLD,CHARGERATE_MAX_THRESHOLD,chargeRate,"ChargeRate");
-  
-  result = temperatureIndicator && socIndicator && chargeRateIndicator;
-  return result;
+float convertTemperatureToCelcius(string temperature)
+{
+	string temperatureValueInString(temperature, 0, temperature.length() - 2);
+	float temperatureInCelcius = 0.0, temperatureValue = 0.0;
+	stringstream ss;
+	ss << temperatureValueInString;
+	ss >> temperatureValue;
+	temperatureInCelcius = temperatureValue;
+	if (temperature.back() == 'F')
+	{
+		temperatureInCelcius = (temperatureValue - 32) * 5 / 9;
+		
+	}
+	return temperatureInCelcius;
 }
 
-void testBatteryStatus(float temperature, float soc, float chargeRate ,bool expectedResult)
+bool batteryIsOk(string temperature, float soc, float chargeRate) {
+	bool temperatureIndicator = false, socIndicator = false, chargeRateIndicator = false, result = false;
+	float temperatureInCelcius = convertTemperatureToCelcius(temperature);
+	temperatureIndicator = checkBatteryFactorStatus(TEMPERATURE_MIN_THRESHOLD, TEMPERATURE_MAX_THRESHOLD, TEMPERATURE_LOWER_TOLERENCE_LIMIT, TEMPERATURE_UPPER_TOLERENCE_LIMIT, temperatureInCelcius, "temperature");
+	socIndicator = checkBatteryFactorStatus(SOC_MIN_THRESHOLD, SOC_MAX_THRESHOLD, SOC_LOWER_TOLERENCE_LIMIT, SOC_UPPER_TOLERENCE_LIMIT, soc, "SOC");
+	chargeRateIndicator = checkBatteryFactorStatus(CHARGERATE_MIN_THRESHOLD, CHARGERATE_MAX_THRESHOLD, CHARGERATE_LOWER_TOLERENCE_LIMIT, CHARGERATE_UPPER_TOLERENCE_LIMIT, chargeRate, "ChargeRate");
+
+	result = temperatureIndicator && socIndicator && chargeRateIndicator;
+	return result;
+}
+
+void testBatteryStatus(string temperature, float soc, float chargeRate, bool expectedResult)
 {
-  assert(batteryIsOk(temperature,soc,chargeRate) == expectedResult);
+	assert(batteryIsOk(temperature, soc, chargeRate) == expectedResult);
 }
 
 int main() {
-  testBatteryStatus(25, 70, 0.7, true);
-  testBatteryStatus(50, 85, 0, false);
-  testBatteryStatus(46, 40, 0.6, false);
-  testBatteryStatus(-1, 30, 0.5, false);
-  testBatteryStatus(40, 19, 0.6, false);
-  testBatteryStatus(39, 81, 0.5, false);
-  testBatteryStatus(40, 50, -0.1, false);
-  testBatteryStatus(40, 50, 0.9, false);
-  testBatteryStatus(0, 20, 0, true);
-  testBatteryStatus(45, 80, 0.8, true);
+	testBatteryStatus("32.0F", 70, 0.7, true);
+	testBatteryStatus("25.0C", 70, 0.7, true);
+	testBatteryStatus("50.0C", 85, 0, false);
+	testBatteryStatus("46.0C", 40, 0.6, false);
+	testBatteryStatus("30.2F", 30, 0.5, false);
+	testBatteryStatus("40.0C", 19, 0.6, false);
+	testBatteryStatus("39.0C", 81, 0.5, false);
+	testBatteryStatus("40.0C", 50, -0.1, false);
+	testBatteryStatus("40.0C", 50, 0.9, false);
+	testBatteryStatus("0.0C", 20, 0, true);
+	testBatteryStatus("45.0C", 80, 0.8, true);
+	testBatteryStatus("22.0C", 70, 0.7, true);
+	testBatteryStatus("50.0C", 85, 0, false);
+	testBatteryStatus("46.0C", 40, 0.6, false);
+	testBatteryStatus("-1.0C", 30, 0.5, false);
+	testBatteryStatus("40.0C", 19, 0.6, false);
+	testBatteryStatus("39.0C", 81, 0.5, false);
+	testBatteryStatus("40.0C", 50, -0.1, false);
+	testBatteryStatus("40.0C", 50, 0.9, false);
+	testBatteryStatus("1.0C", 20, 0, true);
+	testBatteryStatus("44.0C", 80, 0.8, true); 
+	testBatteryStatus("30.0C", 22, 0.7, true);
+	testBatteryStatus("25.0C", 77, 0.7, true);
+	testBatteryStatus("32.0F", 70, 0.1, true);
+	testBatteryStatus("25.0C", 70, 0.7, true);
 }
